d8af92fdff0250f46dd3ee4cfec88898e0ab7eae
Bryce Mecum <petridish@gmail.com>
2024-03-30 19:59:22 -0800

    remove nuxtjs/tailwindcss
    When you use nuxt/ui you don't need this any longer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
     },
   },
 
-  modules: ["@pinia/nuxt", "@nuxt/ui", "@nuxtjs/tailwindcss", "@nuxtjs/seo"],
+  modules: ["@pinia/nuxt", "@nuxt/ui", "@nuxtjs/seo"],
   colorMode: {
     preference: "light",
   },
