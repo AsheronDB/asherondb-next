eac7322862e24f863f7364bd7443f0e81dba2cab
Bryce Mecum <petridish@gmail.com>
2024-03-30 22:36:11 -0800

    Tidy up creature index code
    Before I start copying this around, I want to it to be more in a decent state to start copying.

--- pages/database/npcs/creatures/index.vue
+++ pages/database/npcs/creatures/index.vue
@@ -2,8 +2,6 @@
   <div>
     <PageBreadcrumb />
     <PageHeader />
-
-
     <div>
       <UTable
         :columns
@@ -30,56 +28,42 @@
 </template>
 
 <script setup lang="ts">
-const route = useRoute();
-console.log(route);
+import type { AsyncData } from '#app';
+import { WeenieType } from '~/util/mappings';
+import { getCountOfWeenieByClassId, getWeenies, type CountResponse, type WeenieListingRow } from "~/util/queries/new"
 
-const totalPages = ref(10);
+// Set up pagination
+const totalPages = ref(0); // Set to zero so we know it isn't set
 const page = ref(1);
 const perPage = ref(10);
 
+definePageMeta({
+  title: "Creatures",
+});
 
-const countSql = computed(
-  () =>
-    `SELECT COUNT(class_Id) as total FROM weenie where type = 10`
-);
-
+// Count
 const { data: count } = await useFetch(
   `https://acedb.treestats.net/ace_world_patches.json?_shape=array`,
   {
     key: "creatures-total",
     query: {
-      sql: countSql,
+      sql: getCountOfWeenieByClassId([WeenieType.Creature]),
     },
   }
-);
+) as AsyncData<CountResponse[] , Error>;
 
-totalPages.value = Number(count.value[0].total, 10)
+totalPages.value = Math.round(Number(count.value[0].total) / perPage.value)
 
-
-const sql = computed(
-  () =>
-    `SELECT w.class_Id as wcid, w_string_name.value as name FROM weenie as w LEFT JOIN weenie_properties_string AS w_string_name ON w.class_Id = w_string_name.object_Id AND w_string_name.type = 1 where w.type = 10 LIMIT ${perPage.value} OFFSET ${(Number(page.value) - 1) * perPage.value
-    }`
-);
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// Records
 const { data: creatures, error } = await useFetch(
   `https://acedb.treestats.net/ace_world_patches.json?_shape=array`,
   {
     key: "creatures",
     query: {
-      sql,
-    },
+      sql: computed(() => getWeenies([WeenieType.Creature], page.value, perPage.value))
+    }
   }
-);
-
-
-// const columns = computed(() =>
-//   creatures.value.columns.map((column) => ({
-//     key: column,
-//     label: column,
-//   }))
-// );
+) as AsyncData<WeenieListingRow, Error>;
 
 const columns = [
   {
@@ -92,48 +76,4 @@ const columns = [
     label: "Name",
   },
 ];
-
-// const rows = computed(() => {
-//   // Object.fromEntries(
-//   //   creatures.value.columns.map((key, index) => [
-//   //     key,
-//   //     creatures.value.rows[index],
-//   //   ])
-//   // );
-
-//   return creatures.value.rows.map((row) => {
-//     const obj = Object.fromEntries(
-//       creatures.value.columns.map((key, index) => [key, row[index]])
-//     );
-
-//     // const obj = {
-//     //   name: "Name",
-//     //   id: "id",
-//     // };
-//     return obj;
-//   });
-// });
-
-// [
-//   {
-//     key: "icon",
-//     label: "",
-//     class: "w-[0.1%] text-center",
-//   },
-//   {
-//     key: "name",
-//     label: "Name",
-//   },
-// ];
-
-// const rows = [
-//   {
-//     id: "1234",
-//     name: "Cow",
-//   },
-// ];
-
-definePageMeta({
-  title: "Creatures",
-});
 </script>
