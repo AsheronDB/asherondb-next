e65a86e95a5bb360fe2dd175ba5505f23140a697
Bryce Mecum <petridish@gmail.com>
2024-03-30 18:52:59 -0800

    Remove unneeded pieces from nxut.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,31 +14,4 @@ export default defineNuxtConfig({
   colorMode: {
     preference: "light",
   },
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-  vite: {
-    build: {
-      minify: "terser",
-      terserOptions: {
-        compress: {
-          defaults: true,
-          drop_console: true,
-        },
-        mangle: {
-          eval: true,
-          module: true,
-          toplevel: true,
-          safari10: true,
-          properties: false,
-        },
-        output: {
-          comments: false,
-        },
-      },
-    },
-  },
 });
