75547a9f133b9de94c8c85df8f4b790236d6cb3b
Bryce Mecum <petridish@gmail.com>
2024-04-17 21:26:43 -0800

    WIP: Create search dropdown component

---
+++ components/Form/Search/DropdownRow.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="grid grid-cols-[32px_auto_100px]">
+    <div class="grid place-items-center p-2">{{ props.icon }}</div>
+    <div class="p-2">
+      <NuxtLink
+        to="/"
+        class="text-actan-500 hover:text-actan-500 hover:underline font-bold"
+        >{{ props.name }}</NuxtLink
+      >
+    </div>
+    <div class="grid text-right p-2 text-gray-200 text-opacity-50">
+      {{ props.category }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ icon: string; name: string; category: string }>();
+</script>
